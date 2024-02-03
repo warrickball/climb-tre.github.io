12091ce3e473b1ffb20b8d26ab68f83a9edeaef7
tombch <t.brier@outlook.com>
2024-02-03 12:56:07 +0000

    Updated json2md.py with black formatting and better formatting for restrictions

--- metadata/json2md.py
+++ metadata/json2md.py
@@ -1,48 +1,54 @@
 #!/usr/bin/env python3
 
+import json
 from argparse import ArgumentParser
 
 parser = ArgumentParser()
-parser.add_argument('json_filename', type=str)
-parser.add_argument('-t', '--title', type=str)
-parser.add_argument('-d', '--depth', type=int, default=1,
-                    help="header depth of title (default=1)")
+parser.add_argument("json_filename", type=str)
+parser.add_argument("-t", "--title", type=str)
+parser.add_argument(
+    "-d", "--depth", type=int, default=1, help="header depth of title (default=1)"
+)
 args = parser.parse_args()
 
-import sys
-import json
 
-with open(args.json_filename, 'r') as f:
+with open(args.json_filename, "r") as f:
     j = json.load(f)
 
 required = [
-    ['Field name', 'Data type', 'Description', 'Restrictions'],
-    ['-----']*4,
+    ["Field name", "Data type", "Description", "Restrictions"],
+    ["-----"] * 4,
 ]
 optional = [row[:] for row in required]  # deep copy
 
-for k, v in j['fields'].items():
+for k, v in j["fields"].items():
     restrictions = []
     if v.get("values"):
-        restrictions.append("• Choices: " + ", ".join(['`' + val + '`' for val in v['values']]))
+        restrictions.append(
+            "• Choices: " + ", ".join([f"`{val}`" for val in v["values"]])
+        )
     if v.get("default") is not None:
-        restrictions.append(f"• Default: {v['default']}")
+        restrictions.append("• Default: " + f"`{v['default']}`")
     if v.get("restrictions"):
+        partitions = [restriction.partition(":") for restriction in v["restrictions"]]
         restrictions.extend(
-            f"• {restriction}" for restriction in v["restrictions"]
+            [
+                "• " + ":".join([condition, f"`{value}`"])
+                for condition, _, value in partitions
+            ]
         )
 
-    row = [f"`{k}`", f"`{v['type']}`", v['description'], "\n".join(restrictions)]
-    if v['required']:
+    row = [f"`{k}`", f"`{v['type']}`", v["description"], "\n".join(restrictions)]
+    if v["required"]:
         required.append(row)
     else:
         optional.append(row)
 
 if args.title:
-    print('#'*args.depth + ' ' + args.title)
+    print("#" * args.depth + " " + args.title)
     print()
 
-print('#'*args.depth + '# Required\n')
-print(''.join(['| ' + ' | '.join(row) + ' |\n' for row in required]))
-print('#'*args.depth + '# Optional\n')
-print(''.join(['| ' + ' | '.join(row) + ' |\n' for row in optional]))
+print("#" * args.depth + "# Required\n")
+print("".join(["| " + " | ".join(row) + " |\n" for row in required]))
+print("#" * args.depth + "# Optional\n")
+print("".join(["| " + " | ".join(row) + " |\n" for row in optional]))
